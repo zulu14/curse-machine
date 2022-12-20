703f17672d5a083a4d709b362cf96277b07d6ddc
zulu14 <71033768+zulu14@users.noreply.github.com>
2022-12-20 21:41:13 +0100

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+# Run the Server
+eval ${MODIFIED_STARTUP}
