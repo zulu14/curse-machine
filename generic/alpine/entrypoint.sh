cd316f7297ee5033640defcb633b6537f2a01cd6
zulu14 <71033768+zulu14@users.noreply.github.com>
2023-01-05 18:57:19 +0100

    Update entrypoint.sh

--- generic/alpine/entrypoint.sh
+++ generic/alpine/entrypoint.sh
@@ -35,7 +35,6 @@ cd /home/container || exit 1
 
 # Replace variables in the startup command
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 
 # Run the startup command
 # shellcheck disable=SC2086
