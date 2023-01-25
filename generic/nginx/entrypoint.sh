09299bc4f2fada0d03600a5df3bf0f8e338384fd
Clement Rousseau <clementrse@gmail.com>
2023-01-25 09:45:59 +0100

    fix with seed

--- generic/nginx/entrypoint.sh
+++ generic/nginx/entrypoint.sh
@@ -6,7 +6,6 @@ export TZ
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-echo "CurseOs debug 1 \n CurseOs debug 2"
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
